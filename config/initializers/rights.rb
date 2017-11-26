29b23f842274037d020f12ae5222c767a8c29134
Bernd Schmidt <bernd.schmidt@quantenradierer.de>
2017-11-26 01:07:28 +0100

    adds the a right enum

---
+++ config/initializers/rights.rb
@@ -0,0 +1,20 @@
+
+RIGHTS = {
+    :show => 1,
+    :update => 2,
+    :upgrade => 4,
+    :upgrade_lgsm => 8,
+    :start => 16,
+    :stop => 32,
+    :console => 64,
+    :backup => 128,
+    :logs => 256,
+    :edit_config_low => 512,
+    # low important config files, like the game mode
+
+    :edit_config_medium => 1024,
+    # medium important config files, like the game operators
+
+    :edit_config_high => 2028,
+    # high important config files, like the used memory, port, and game administrators
+}
